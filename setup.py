ffa97b47a37956935846b771bbf107c18a646e5a
chp2001 <chp.2001.jhp@gmail.com>
2025-08-25 09:26:17 -0500

    Increment project version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ Then, you can use it in your Python code:
 """
 
 # Other information
-VERSION = "1.0.3"
+VERSION = "1.0.4"
 DESCRIPTION = "URL generator tool for National Water Model data"
 
 setup(
